978399d6fb4b31f131638a35e95e1f2a772d245c
Payrouse Nicolas <wawawoom@gmail.com>
2025-12-09 21:54:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const Browser = require("zombie");
+
+var url =
+  "https://wawawoom.fr/geocaching/GC8GG1R/step2-dc3504d9-7d66-4a9d-982b-e161b02932d5/administrator.php";
+
+var browser = new Browser();
+
+browser.setCookie({ name: "username", domain: "wawawoom.fr", value: "nikolo" });
+browser.setCookie({
+  name: "password",
+  domain: "wawawoom.fr",
+  value: "olokin123!",
+});
+
+browser.visit(url, function (err, _browser, status) {
+  if (err) {
+    throw err.message;
+  }
+});
+
+browser.visit(url2, function (err, _browser, status) {
+  if (err) {
+    throw err.message;
+  }
+});
